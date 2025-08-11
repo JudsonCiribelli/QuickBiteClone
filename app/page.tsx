6e2593844a3d645ec4fa37431ce3ef82312a8dfb
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-08-10 22:38:01 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryListComponent from "./_components/Category-List-Component/categoryListComponent";
 import HeaderComponent from "./_components/Header-Component/headerComponent";
 import SearchComponent from "./_components/Search-Component/searchComponent";
@@ -11,6 +12,17 @@ const Home = () => {
       </div>
       <div className="px-5 pt-6">
         <CategoryListComponent />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/Banner.png"
+          alt="até 30% de descontos em pizza"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
