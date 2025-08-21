4cd08de0b5a6f5080b6163888f06be5637996969
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-08-20 22:13:57 -0300

    feat: add restaurant image component

---
+++ app/restaurants/[id]/_components/Restaurant-Image/restaurantImage.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Button } from "@/app/_components/ui/button";
+import { Restaurant } from "@/app/generated/prisma";
+import { ChevronLeftIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+interface RestaurantImageProps {
+  restaurant: Pick<Restaurant, "name" | "imageUrl">;
+}
+
+const RestaurantImage = ({ restaurant }: RestaurantImageProps) => {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.back();
+  };
+  return (
+    <div className="relative h-[360px] w-full">
+      <Image
+        src={restaurant.imageUrl}
+        alt={restaurant.name}
+        fill
+        className="object-cover"
+      />
+      <Button
+        onClick={handleBackClick}
+        size="icon"
+        className="absolute left-4 top-4 rounded-full bg-white text-foreground hover:text-white"
+      >
+        <ChevronLeftIcon size={20} />
+      </Button>
+    </div>
+  );
+};
+
+export default RestaurantImage;
