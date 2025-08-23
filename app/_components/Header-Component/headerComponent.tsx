6514b88f0ea9f87913443e5e410d4acdea0e865e
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-08-23 11:59:15 -0300

    feat: redirect user to home page

--- app/_components/Header-Component/headerComponent.tsx
+++ app/_components/Header-Component/headerComponent.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const HeaderComponent = () => {
   return (
-    <header className="flex justify-between px-4 pt-6">
-      <Image src="/logo.png" width={120} height={30} alt="logo" />
-      <Button variant="secondary" size="icon">
-        <MenuIcon />
-      </Button>
-    </header>
+    <Link href="/">
+      <header className="flex justify-between px-4 pt-6">
+        <Image src="/logo.png" width={120} height={30} alt="logo" />
+        <Button variant="secondary" size="icon">
+          <MenuIcon />
+        </Button>
+      </header>
+    </Link>
   );
 };
 
