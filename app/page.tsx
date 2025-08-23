38c2e83b0297bfbfd3c72caa6ed1c74c5916a70e
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-08-23 12:25:18 -0300

    refactor: move link inside button

--- app/page.tsx
+++ app/page.tsx
@@ -44,15 +44,15 @@ const Home = async () => {
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
-          <Link href={`/products/recommended`}>
-            <Button
-              variant="ghost"
-              className="h-fit p-0 font-semibold text-primary hover:bg-transparent"
-            >
-              Ver Todos
-              <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 font-semibold text-primary hover:bg-transparent"
+            asChild
+          >
+            <Link href={`/products/recommended`}>
+              Ver Todos <ChevronRightIcon size={16} />{" "}
+            </Link>
+          </Button>
         </div>
         <ProductListComponent products={products} />
       </div>
@@ -64,15 +64,16 @@ const Home = async () => {
       <div className="space-y-4 py-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
-          <Link href={`/restaurants/recommended`}>
-            <Button
-              variant="ghost"
-              className="h-fit p-0 font-semibold text-primary hover:bg-transparent"
-            >
-              Ver Todos
-              <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+
+          <Button
+            variant="ghost"
+            className="h-fit p-0 font-semibold text-primary hover:bg-transparent"
+            asChild
+          >
+            <Link href={`/restaurants/recommended`}>
+              Ver Todos <ChevronRightIcon size={16} />
+            </Link>
+          </Button>
         </div>
         <RestaurantListComponent />
       </div>
