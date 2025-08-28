61faad1d09acf5a9a4a798b3a0fd0d16cabebdbd
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-08-27 21:20:42 -0300

    feat: add Cart component

--- app/_components/Cart-Component/cartComponent.tsx
+++ app/_components/Cart-Component/cartComponent.tsx
@@ -1,9 +1,14 @@
 import { CartContext } from "@/app/_context/cart";
 import { useContext } from "react";
 import CartItem from "../Cart-Item/cartItem";
+import { Card, CardContent } from "../ui/card";
+import { formatCurrency } from "@/app/_helpers/price";
+import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
 
 const CartComponent = () => {
-  const { products } = useContext(CartContext);
+  const { products, totalDiscounts, totalPrice, subTotalPrice } =
+    useContext(CartContext);
   return (
     <div className="py-5">
       <div className="space-y-4">
@@ -11,6 +16,46 @@ const CartComponent = () => {
           <CartItem key={product.name} cartProduct={product} />
         ))}
       </div>
+
+      <div className="mt-6">
+        <Card>
+          <CardContent className="space-y-4 p-5">
+            <div className="flex items-center justify-between">
+              <span className="text-sm text-muted-foreground">Subtotal</span>
+              <span className="text-sm font-semibold">
+                {formatCurrency(subTotalPrice)}
+              </span>
+            </div>
+            <Separator />
+
+            <div className="flex items-center justify-between">
+              <span className="text-sm text-muted-foreground">Descontos</span>
+              <span className="text-sm font-semibold">
+                - {formatCurrency(totalDiscounts)}
+              </span>
+            </div>
+            <Separator />
+
+            <div className="flex items-center justify-between">
+              <span className="text-sm text-muted-foreground">Entrega</span>
+              <span className="text-sm font-semibold text-primary">
+                {Number(products[0].restaurant.deliveryFee) === 0
+                  ? "Grátis"
+                  : formatCurrency(Number(products[0].restaurant.deliveryFee))}
+              </span>
+            </div>
+            <Separator />
+
+            <div className="flex items-center justify-between">
+              <span className="text-sm font-semibold">Total</span>
+              <span className="text-sm font-semibold">
+                {formatCurrency(totalPrice)}
+              </span>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      <Button className="mt-6 w-full">Finalizar pedido</Button>
     </div>
   );
 };
---
+++ app/_context/cart.tsx
@@ -0,0 +1,154 @@
+"use client";
+import { createContext, ReactNode, useMemo, useState } from "react";
+import { Prisma } from "../generated/prisma";
+import { calculateProductTotalPrice } from "../_helpers/price";
+
+export interface CartProduct
+  extends Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          deliveryFee: true;
+        };
+      };
+    };
+  }> {
+  quantity: number;
+}
+interface ICartContext {
+  products: CartProduct[];
+  subTotalPrice: number;
+  totalPrice: number;
+  totalDiscounts: number;
+  handleAddProductToCart: (
+    product: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            deliveryFee: true;
+          };
+        };
+      };
+    }>,
+    quantity: number,
+  ) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
+  removeProductFromCart: (productId: string) => void;
+}
+
+export const CartContext = createContext<ICartContext>({
+  products: [],
+  subTotalPrice: 0,
+  totalPrice: 0,
+  totalDiscounts: 0,
+  handleAddProductToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProductFromCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subTotalPrice = useMemo(() => {
+    return products.reduce((acc, products) => {
+      return acc + Number(products.price) * products.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalPrice = useMemo(() => {
+    return products.reduce((acc, products) => {
+      return acc + calculateProductTotalPrice(products) * products.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscounts = subTotalPrice - totalPrice;
+
+  const decreaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          if (cartProduct.quantity === 1) {
+            return cartProduct;
+          }
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity - 1,
+          };
+        }
+        return cartProduct;
+      }),
+    );
+  };
+
+  const increaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+        return cartProduct;
+      }),
+    );
+  };
+
+  const removeProductFromCart = (productId: string) => {
+    return setProducts((prev) =>
+      prev.filter((product) => product.id !== productId),
+    );
+  };
+
+  const handleAddProductToCart = (
+    product: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            deliveryFee: true;
+          };
+        };
+      };
+    }>,
+    quantity: number,
+  ) => {
+    const isProductAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (isProductAlreadyOnCart) {
+      return setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity,
+            };
+          }
+          return cartProduct;
+        }),
+      );
+    }
+
+    setProducts((prev) => [...prev, { ...product, quantity: quantity }]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        subTotalPrice,
+        totalDiscounts,
+        totalPrice,
+        handleAddProductToCart,
+        decreaseProductQuantity,
+        increaseProductQuantity,
+        removeProductFromCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
