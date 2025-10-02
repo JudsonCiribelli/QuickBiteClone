196865bb063893c393ea89cfddea7c90e5997fa8
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-02 20:13:40 -0300

    feat: add order page

---
+++ app/my-orders/page.tsx
@@ -0,0 +1,45 @@
+import { getServerSession } from "next-auth";
+import db from "../_lib/prisma";
+import { authOptions } from "../_lib/auth";
+import { redirect } from "next/navigation";
+import HeaderComponent from "../_components/Header-Component/headerComponent";
+import OrderItemComponent from "./_components/Order-Item-Component/orderItemComponent";
+
+const MyOrdersPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  const orders = await db.order.findMany({
+    where: {
+      userId: session.user.id,
+    },
+    include: {
+      restaurant: true,
+      products: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <HeaderComponent />
+
+      <div className="px-5 py-6">
+        <h2 className="text-lg font-semibold">Meus pedidos</h2>
+        <div className="space-y-3">
+          {orders.map((order) => (
+            <OrderItemComponent key={order.id} order={order} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MyOrdersPage;
