ceab71797a3c7daf683eab1bd0bc1220c5857a2e
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-01 21:17:02 -0300

    chore: ensure cache will be cleared after creating an order

--- app/_actions/order.ts
+++ app/_actions/order.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import db from "../_lib/prisma";
 import { Prisma } from "../generated/prisma";
 
 export const createOrder = async (data: Prisma.OrderCreateInput) => {
-  return await db.order.create({ data });
+  await db.order.create({ data });
+  revalidatePath("/my-orders");
 };
